b2c2ac7aa35bf514478e3963b145f849b3d23147
junkyman2972 <yow79472@gmail.com>
2024-10-10 20:01:30 -0500

    final code before meet 0

--- FTC-2024-2025-master (1)/FTC-2024-2025-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/Testing.java
+++ FTC-2024-2025-master (1)/FTC-2024-2025-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/Testing.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode.TeleOP;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(group = "TeleOp", name = "Test")
-@Disabled
 public class Testing extends LinearOpMode {
 
     DcMotorEx LeftT;
@@ -15,15 +15,19 @@ public class Testing extends LinearOpMode {
     DcMotorEx LeftB;
     DcMotorEx RightB;
 
+    DcMotorEx HorizontalIntake;
+
     Servo Intake1;
     Servo Intake2;
+
+    CRServo Intake;
 
     double DriveSpeed = 0.4;
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_POS     =  0.25;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;
+    static final double MAX_POS     =  0.5;     // Maximum rotational position
+    static final double MIN_POS     =  0.25;
 
     double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
     boolean rampUp = true;
@@ -36,6 +40,8 @@ public class Testing extends LinearOpMode {
         RightB = hardwareMap.get(DcMotorEx.class, "RightB");
         Intake1 = hardwareMap.get(Servo.class, "Intake1");
         Intake2 = hardwareMap.get(Servo.class, "Intake2");
+        Intake = hardwareMap.get(CRServo.class, "Intake");
+        HorizontalIntake = hardwareMap.get(DcMotorEx.class, "HorizontalIntake");
 
         Intake1.setDirection(Servo.Direction.REVERSE);
 
@@ -48,22 +54,20 @@ public class Testing extends LinearOpMode {
             GP1Controls();
             Servo();
             ContinueServo();
+            HoriIntake();
         }
     }
     public void GP1Controls () {
         LeftT.setPower(DriveSpeed * (gamepad1.left_stick_y) - (gamepad1.left_stick_x) - (gamepad1.right_stick_x));
-        LeftB.setPower(DriveSpeed * (gamepad1.left_stick_y) + (gamepad1.left_stick_x) - (gamepad1.right_stick_x));
+        LeftB.setPower(DriveSpeed * (gamepad1.left_stick_y) - (gamepad1.left_stick_x) + (gamepad1.right_stick_x));
         RightT.setPower(DriveSpeed * (-gamepad1.left_stick_y) - (gamepad1.left_stick_x) - (gamepad1.right_stick_x));
-        RightB.setPower(DriveSpeed * (-gamepad1.left_stick_y) + (gamepad1.left_stick_x) - (gamepad1.right_stick_x));
+        RightB.setPower(DriveSpeed * (-gamepad1.left_stick_y) - (gamepad1.left_stick_x) + (gamepad1.right_stick_x));
 
-        if (gamepad1.right_bumper) {
-            DriveSpeed = 1;
-        }
-        if (gamepad1.left_bumper) {
-            DriveSpeed = 0.4;
-        }
+        //DriveSpeed = (1 * gamepad1.right_trigger) + (0.4 * gamepad1.left_trigger);
 
     }
+
+    //set to Servo Port 0 and 1
     public void Servo () {
         if (rampUp) {
             // Keep stepping up until we hit the max value.
@@ -88,7 +92,21 @@ public class Testing extends LinearOpMode {
             Intake1.setPosition(-position);
         }
     }
-    public void ContinueServo () {
 
+    //set to Servo Port 2
+    public void ContinueServo () {
+        Intake.setPower(0.5 * -(gamepad2.right_stick_y) + (gamepad2.right_stick_y));
+        /*if (gamepad2.dpad_up) {
+            Intake.setPower(0.5);
+        }
+        else if (gamepad2.dpad_down) {
+            Intake.setPower(-0.5);
+        }
+        else {
+            Intake.setPower(0);
+        }*/
+    }
+    public void HoriIntake () {
+        HorizontalIntake.setPower(0.5 * (gamepad2.right_stick_x) - (gamepad1.right_stick_x));
     }
 }
